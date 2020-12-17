4651b4ae86fdee7da1c23cacc44afb909eeb8d67
weekbelt <vfrvfr4207@gmail.com>
2020-12-17 20:56:45 +0900

    Initial commit

---
+++ src/test/java/me/weekbelt/springbootcommunityweb/SpringBootCommunityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.weekbelt.springbootcommunityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommunityWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
